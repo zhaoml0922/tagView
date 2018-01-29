c24a4721d3446358a970b4785fafa32d17edc602
zhaoml <zml_xrzs@163.com>
2018-01-29 15:12:48 +0800

    first commit

---
+++ TagButton/FlagsView.h
@@ -0,0 +1,13 @@
+//
+//  FlagsView.h
+//  TagButton
+//
+//  Created by zhaoml on 2017/4/6.
+//  Copyright © 2017年 赵明亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlagsView : UIView
+
+@end
---
+++ TagButton/FlagsView.m
@@ -0,0 +1,21 @@
+//
+//  FlagsView.m
+//  TagButton
+//
+//  Created by zhaoml on 2017/4/6.
+//  Copyright © 2017年 赵明亮. All rights reserved.
+//
+
+#import "FlagsView.h"
+
+@implementation FlagsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
